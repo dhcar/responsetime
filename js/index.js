b12f5bf12632219ab1af02ece6be2d6420164e98
David Helgeson <davidhelgeson@Davids-MacBook-Pro.local>
2015-08-19 23:11:45 -0700

    variance test added

--- js/index.js
+++ js/index.js
@@ -1,36 +1,94 @@
-var button = document.getElementById('button');
-var resultsText = document.getElementById('resultsText');
+var button         = document.getElementById('button');
+
+var varianceText   = document.getElementById('variance');
+var varianceButton = document.getElementById('varianceButton');
+
+var resultsText    = document.getElementById('resultsText');
+
+var body           = document.getElementsByTagName('body')[0];
+
+var timer          = window.performance || Date;
 
 var start = 0;
 
-button.addEventListener('click', function(){
+button.addEventListener('click', startTimer);
+
+function startTimer(){
   resultsText.textContent = '';
   start = 0;
   
   var onsetTime = getRandomArbitrary(3000, 6000);
   
   window.setTimeout(function(){
-    start = Date.now();
+    start = timer.now();
     setBackgroundRed();
   }, onsetTime);
   
-});
+}
 
-document.getElementsByTagName('body')[0].addEventListener('keydown', function(){
-  if(start){
-    var time = Date.now() - start;
+body.addEventListener('keydown', stopTimer);
+
+function stopTimer(event){
+  if(start > 0){
+    var time = timer.now() - start;
     setBackgroundWhite();
     resultsText.textContent = time + " milliseconds";
     start = 0;
+    // stop space bar from activating button again
+    event.preventDefault();
+    event.stopPropagation();
   }
-});
+  console.log(event);
+}
+
+// body.addEventListener('mousedown', stopTimer);
+
+// 
+// run on init to establish variance
+// 
+
+var varianceStart = 0;
+
+
+var flarp = document.createEvent('Event');
+
+flarp.initEvent('flarp', true, true);
+
+varianceText.addEventListener('flarp', varianceEnd);
+
+function establishVariance(){
+
+  varianceStart = 0;
+
+  window.setTimeout(function(){
+    varianceStart = timer.now();
+    setBackgroundRed();
+    if(body.style.backgroundColor == 'red') varianceText.dispatchEvent(flarp);
+    // varianceEnd();
+  },0);
+
+}
+
+function varianceEnd(){
+  if(varianceStart > 0){
+    var time = timer.now() - varianceStart;
+    setBackgroundWhite();
+    varianceText.textContent = "variance: " + time + " milliseconds";
+  }
+}
+
+// varianceButton.addEventListener('mouseup', varianceEnd);
+varianceButton.addEventListener('mousedown', establishVariance);
+
+// establishVariance()
+
 
 function setBackgroundRed(){
-  document.getElementsByTagName('body')[0].style.backgroundColor = 'red';
+  body.style.backgroundColor = 'red';
 }
 
 function setBackgroundWhite(){
-   document.getElementsByTagName('body')[0].style.backgroundColor = 'white';
+   body.style.backgroundColor = 'white';
 }
 
 function getRandomArbitrary(min, max) {
